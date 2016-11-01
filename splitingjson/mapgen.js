d2d56d38d097851c8a21c77fdca81c1183967eee
nghianguyentran92 <nghianguyentran92@gmail.com>
2016-11-01 23:11:34 +0700

    commmit file mapgen.js

--- splitingjson/mapgen.js
+++ splitingjson/mapgen.js
@@ -1,18 +1,45 @@
 const ljf = require( "load-json-file" );
 const wjf = require( "write-json-file" );
+const southid = "T6shP0GyRuQ";
+const northid = "JzKStMojAGA";
+const highlandid = "bJ55F4lb3WJ";
+const centralid = "SgkhNupCR4x";
 
-const oul3 = ljf.sync( "./data/vnProv.json" );
-var o={features:[]
+let north={features:[]
+};
+let central={features:[]
+};
+let highland={features:[]
+};
+let south={features:[]
 };
 
+const loadjsonfile = () => {
+  return new Promise(function(res, rej){
+      res(ljf.sync("./data/vnProv.json"));
+  });
+};
 
-oul3.features.forEach(function(features){
-	///change the uid to region north JzKStMojAGA, central SgkhNupCR4x, highland bJ55F4lb3WJ, south T6shP0GyRuQ
-  if(features.properties.parent=='T6shP0GyRuQ'){
-   console.log(features.id);
-    o.features.push(features);
-
-  }
-});
-/// change the outputfile name based on the region
-wjf.sync( "./data/southregionmap.json", o, {indent: 2} );
+loadjsonfile().then( result => {
+    result.features.forEach(function(features){
+        if(features.properties.parent == southid){
+                south.features.push(features);
+        }
+        if(features.properties.parent == centralid){
+                central.features.push(features);
+        }
+        if(features.properties.parent == highlandid){
+                highland.features.push(features);
+        }
+        if(features.properties.parent == northid){
+                north.features.push(features);
+        }
+    } );
+    }
+ )
+ .then( function() {
+       wjf.sync( "./data/southregionmap.json", south, {indent: 2} );
+       wjf.sync( "./data/centralregionmap.json", central, {indent: 2} );
+       wjf.sync( "./data/highlandregionmap.json", highland, {indent: 2} );
+       wjf.sync( "./data/northregionmap.json", north, {indent: 2} );
+ } );
